46552f40f3dc6aec89acf54d9a2a83021bbdac67
fkilld <fkill.d.azad@gmail.com>
2020-09-25 22:19:48 +0530

    my commt

--- carzone/settings.py
+++ carzone/settings.py
@@ -153,5 +153,5 @@ SITE_ID = 1
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'somethingnew4200w@gmail.com'
-EMAIL_HOST_PASSWORD = '!QAZXSW@'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
